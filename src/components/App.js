c09f56229a6d24f32c1d0dab3594da91c3e3a244
Daniel Laserna <daniel.laserna@protonmail.com>
2021-02-08 15:08:23 -0500

    Add base Dashboard with routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { handleInitialData } from "../actions/shared"
-import Leaderboard from '../components/Leaderboard'
+import { BrowserRouter as Router } from "react-router-dom";
+import { handleInitialData } from "../actions/shared";
+import Leaderboard from "../components/Leaderboard";
+import Dashboard from "../components/Dashboard";
 
 export default function App() {
   const store = useSelector((store) => store);
-  const dispatch = useDispatch()
-  const loading = useSelector(state => state.authedUser === null)
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authedUser === null);
 
   useEffect(() => {
-    dispatch(handleInitialData())
-
-  }, [dispatch]) 
-  console.log('Store', store)
+    dispatch(handleInitialData());
+  }, [dispatch]);
+  console.log("Store", store);
 
   return (
-    <div className='container'>
-      {loading === false && <Leaderboard />}
-
-    </div>
+    <Router>
+      <div className="container">{loading === false && <Dashboard />}</div>
+    </Router>
   );
 }
